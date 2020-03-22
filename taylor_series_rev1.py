4fc3a8d3a158cff98a5e08a741afe44165081b1a
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-03-22 23:35:46 +0700

    Update taylor_series_rev1.py

--- taylor_series_rev1.py
+++ taylor_series_rev1.py
@@ -4,7 +4,7 @@
 # Dhava Gautama
 # Thanks to Allah, My Parents, Bu Fitri, StackOverflow, GeeksforGeeks, SciPy Docs, JournalDev, Programiz
 # https://github.com/dhava-stmkg/taylor_series
-# Versi 0.1.2
+# Versi 0.1.3
 
 import numpy as np
 import sympy as sy
@@ -24,16 +24,26 @@ f = sin(x)
 def taylor(fungsi, x0, n,nilai_x):
     i = 0
     p = 0
+    tailor = []
     while i <= n:
         # fungsi.diff(x,i) => Calculate i-th order derivative of the function on x
         # subs(x,x0) => Subs the value of x with x0
         p = p + (fungsi.diff(x,i).subs(x,x0))/(factorial(i))*(x-x0)**i
+        tailor.append(p.subs(x,nilai_x))
         i += 1
     hasil_pendekatan = np.float(p.subs(x,nilai_x))
     true_value = np.float(fungsi.subs(x,nilai_x))
-    true_error = np.float(hasil_pendekatan-true_value)
+    true_error = np.float(true_value-hasil_pendekatan)
     relative_true_error = np.float(true_error/true_value*100)
-    return [hasil_pendekatan,true_value,true_error,relative_true_error]
+    # Diperlukan untuk mendapatkan nilai approx-error
+    tailor = list(dict.fromkeys(tailor)) # Menghapus nilai duplikat pada array
+    tailor.reverse() # Membalikan urutan array
+    if 0 in tailor: # Jika ditemukan nilai nol dalam array
+        tailor.remove(0) # Menghapus nilai nol
+    # Menghitung nilai approx error
+    app_err = np.float(tailor[0]-tailor[1]) # Nilai approximate error
+    rel_app_err = app_err/tailor[0]*100 # Nilai relative approximate error
+    return [hasil_pendekatan,true_value,true_error,relative_true_error,app_err,rel_app_err]
 
 # Dekati sin(x) ketika x0=0, dan x=1 pada iterasi ke n=10 masukan ke kamar gas
 gas = taylor(f, 0, 10, 1)
@@ -41,3 +51,5 @@ print('Nilai Pendekatan =',gas[0])
 print('Nilai True Value =',gas[1])
 print('Nilai True Error =',gas[2])
 print('Nilai Relative True Error =',gas[3],'%')
+print('Nilai Approximate Error =',gas[4])
+print('Nilai Relative Approximate Error =',gas[5],'%')
